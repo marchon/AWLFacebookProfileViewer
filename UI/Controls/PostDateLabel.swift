529fb40a1f4f7256732afb365b7be313ac4f38d1
Volodymyr Gorlov <Volodymyr.Gorlov@tomtom.com>
2015-03-24 17:22:57 +0200

    Corrected color usage for post date color.

--- UI/Controls/PostDateLabel.swift
+++ UI/Controls/PostDateLabel.swift
@@ -8,7 +8,7 @@ import UIKit
 public class PostDateLabel : IBDesignableLabel {
 
   override func setupNib() {
-    self.textColor = SketchStyleKit.paletteColor4Fill.fillColor
+    self.textColor = SketchStyleKit.postDate.textColor
   }
 
 }
